75c856627386bb0ca38290913caac06e470d94a8
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2021-01-06 13:52:56 +0530

    Re-factor product routes using controllers

---
+++ backend/controllers/productControllers.js
@@ -0,0 +1,20 @@
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
+
+// Description 	: Returns all products
+// Route 		: /api/products
+export const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+});
+
+// Description 	: Returns single product
+// Route 		: /api/product
+export const getProductsByID = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) res.json(product);
+    else {
+        res.status(404);
+        throw new Error("Product not found");
+    }
+});
