5736b7d6cb93c4cc8be98d0839404d344a590748
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2021-01-19 21:36:25 +0530

    Add route to retrieve paypal info

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import logger from "./logger.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import paypalRoutes from "./routes/paypalRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 // Load environment variables
@@ -36,6 +37,7 @@ app.use(
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/paypal", paypalRoutes);
 
 // Custom error handling middleware
 app.use(notFound);
