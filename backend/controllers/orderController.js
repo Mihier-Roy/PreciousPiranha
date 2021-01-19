840ae96ebc43f389d3db53f872782f747b1d6f73
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2021-01-19 20:28:23 +0530

    Add route to set order as paid

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -48,3 +48,25 @@ export const getOrderById = asyncHandler(async (req, res) => {
         throw new Error("Order not found");
     }
 });
+
+// Description 	: Sets isPaid to true and adds paymentResult info from payment gateway being used
+// Route 		: PUT /api/orders/:id/pay
+export const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+        order.isPaid = true;
+        order.paidAt = Date.now();
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address
+        };
+
+        const updatedOrder = await order.save();
+        res.json(updatedOrder);
+    } else {
+        res.status(404);
+        throw new Error("Order not found");
+    }
+});
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { addOrderItems, getOrderById } from "../controllers/orderController.js";
+import { addOrderItems, getOrderById, updateOrderToPaid } from "../controllers/orderController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
 router.route("/").post(verifyToken, addOrderItems);
 router.route("/:id").get(verifyToken, getOrderById);
+router.route("/:id/pay").put(verifyToken, updateOrderToPaid);
 
 export default router;
