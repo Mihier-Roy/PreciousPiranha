5298723e6617e68a93cf132afc0491a2fcaca488
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2022-07-28 11:26:12 +0100

    Converted to ts

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -40,21 +40,18 @@ const reducer = combineReducers({
 
 // Check local storage for cartItems
 // If cartItems are present, set the initial state to the values saved in local storage
-const cartItemsFromLocalStorage = localStorage.getItem("cartItems")
-    ? JSON.parse(localStorage.getItem("cartItems"))
-    : [];
+const cartItems: string|null = localStorage.getItem("cartItems")
+const cartItemsFromLocalStorage = cartItems != null ? JSON.parse(cartItems) : [];
 
 // Check local storage for userInfo
-// If userInfo is set, set the initial state to the values saved in local storage
-const userInfoFromLocalStorage = localStorage.getItem("userInfo")
-    ? JSON.parse(localStorage.getItem("userInfo"))
-    : null;
+// If userInfo is set, set the initial state to the values saved in local 
+const userInfo: string|null = localStorage.getItem("userInfo")
+const userInfoFromLocalStorage = userInfo != null ? JSON.parse(userInfo) : null;
 
 // Check local storage for shippingAddress
 // If shippingAddress is set, set the initial state to the values saved in local storage
-const shippingAddressFromLocalStorage = localStorage.getItem("shippingAddress")
-    ? JSON.parse(localStorage.getItem("shippingAddress"))
-    : {};
+const shippingAddress = localStorage.getItem("shippingAddress")
+const shippingAddressFromLocalStorage = shippingAddress ? JSON.parse(shippingAddress) : {};
 
 const initialState = {
     cart: {
