159671c4062872ea9c5e84de1479c20c184d02bc
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2022-07-30 16:41:24 +0100

    Added type declaration file for custom types

---
+++ frontend/src/types.d.ts
@@ -0,0 +1,56 @@
+type ProductItem = {
+    user: string;
+    name: string;
+    image: string;
+    description: string;
+    brand: string;
+    category: string;
+    price: number;
+    countInStock: number;
+    rating: number;
+    numReviews: number;
+    reviews: Reviews[];
+};
+
+type Reviews = {
+    name: string;
+    rating: number;
+    comment: string;
+};
+
+type CartItem = {
+    name: string;
+    quantity: number;
+    image: string;
+    price: number;
+    productID: number;
+};
+
+type ShippingAddress = {
+    address: string;
+    city: string;
+    postalCode: string;
+    country: string;
+};
+
+type PaymentResult = {
+    id: string;
+    status: string;
+    update_time: string;
+    email_address: string;
+};
+
+type OrderDetails = {
+    user: string;
+    orderItems: CartItem[];
+    shippingAddress: ShippingAddress;
+    paymentMethod: string;
+    paymentResult: PaymentResult;
+    taxPrice: number;
+    shippingPrice: number;
+    totalPrice: number;
+    isPaid: boolean;
+    paidAt: Date | null;
+    isDelivered: boolean;
+    deliveredAt: Date | null;
+};
