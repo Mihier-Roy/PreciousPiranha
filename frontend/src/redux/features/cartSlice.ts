5303967d017bc9e49a9d8e93c84a5c32aa37e80e
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2022-07-30 15:25:29 +0100

    Re-factored to use the auto-generated actions

--- frontend/src/redux/features/cartSlice.ts
+++ frontend/src/redux/features/cartSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import axios from "axios";
 
 type CartItem = {
@@ -32,27 +32,50 @@ export const cartSlice = createSlice({
     name: "cart",
     initialState,
     reducers: {
-        addItem: (state: CartState, action: PayloadAction<CartItem>) => {
-            const newItem = action.payload;
-            const itemExists = state.cartItems.find((item) => item.productID === newItem.productID);
-            // If the item exists, replace the item in the cart with the new item
-            // Ensures any updates made will be reflected in the cart
-            if (itemExists) {
-                state.cartItems = state.cartItems.map((item) =>
-                    item.productID === newItem.productID ? newItem : item
+        addItem: {
+            reducer(state: CartState, action: PayloadAction<CartItem>) {
+                const newItem = action.payload;
+                const itemExists = state.cartItems.find(
+                    (item) => item.productID === newItem.productID
                 );
-            } else {
-                state.cartItems.push(newItem);
+                // If the item exists, replace the item in the cart with the new item
+                // Ensures any updates made will be reflected in the cart
+                if (itemExists) {
+                    state.cartItems = state.cartItems.map((item) =>
+                        item.productID === newItem.productID ? newItem : item
+                    );
+                } else {
+                    state.cartItems.push(newItem);
+                }
+
+                // Save the cartItems array to local storage so that it may be accessed again
+                // even if the user navigates away from the page
+                localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+            },
+            prepare({ data, quantity }) {
+                return {
+                    payload: {
+                        productID: data._id,
+                        name: data.name,
+                        image: data.image,
+                        price: data.price,
+                        countInStock: data.countInStock,
+                        quantity: quantity
+                    }
+                };
             }
         },
         removeItem: (state: CartState, action: PayloadAction<number>) => {
             state.cartItems = state.cartItems.filter((item) => item.productID !== action.payload);
+            localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
         },
         saveShippingAddress: (state: CartState, action: PayloadAction<ShippingAddress>) => {
             state.shippingAddress = action.payload;
+            localStorage.setItem("shippingAddress", JSON.stringify(action.payload));
         },
         savePaymentMethod: (state: CartState, action: PayloadAction<string>) => {
             state.paymentMethod = action.payload;
+            localStorage.setItem("paymentMethod", JSON.stringify(action.payload));
         }
     }
 });
@@ -60,40 +83,8 @@ export const cartSlice = createSlice({
 export const { addItem } = cartSlice.actions;
 export default cartSlice.reducer;
 
-// Cart Actions
-export const addItemToCart = (id, quantity) => async (dispatch, getState) => {
-    // Get product information from the API
-    const { data } = await axios.get(`/api/products/${id}`);
-
-    // Dispatch product information to the cartReducer where it will be added to the cartItems array
-    dispatch({
-        type: "cart/addItem",
-        payload: {
-            productID: data._id,
-            name: data.name,
-            image: data.image,
-            price: data.price,
-            countInStock: data.countInStock,
-            quantity: quantity
-        }
-    });
-
-    // Save the cartItems array to local storage so that it may be accessed again even if the user navigates away from the page
-    localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
-};
-
-export const removeItemFromCart = (id) => (dispatch, getState) => {
-    dispatch({ type: "cart/removeItem", payload: id });
-    // Update the local storage cartItems value using the state after the product has been removed from the cart
-    localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
-};
-
-export const saveShippingAddress = (data) => (dispatch) => {
-    dispatch({ type: "cart/saveAddress", payload: data });
-    localStorage.setItem("shippingAddress", JSON.stringify(data));
-};
-
-export const savePaymentMethod = (data) => (dispatch) => {
-    dispatch({ type: "cart/savePaymentMethod", payload: data });
-    localStorage.setItem("paymentMethod", JSON.stringify(data));
-};
+// Get product information from the API
+export const addItemToCart = createAsyncThunk("cart/addItem", async (id, quantity) => {
+    const response = await axios.get(`/api/products/${id}`);
+    return { data: response.data, quantity };
+});
