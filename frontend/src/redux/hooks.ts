55c80ffb36621fb6e97175244fa12e904bbe08bb
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2022-07-31 18:17:53 +0100

    Fixed type declaration of useAppDispatch

--- frontend/src/redux/hooks.ts
+++ frontend/src/redux/hooks.ts
@@ -2,5 +2,5 @@ import { TypedUseSelectorHook, useSelector, useDispatch } from "react-redux";
 import type { RootState, AppDispatch } from "./store";
 
 // Allow Redux to know the types of objects returned by useDispatch and useSelector
-export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
