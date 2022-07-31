053de446a97f7ca46df38bbb2c63bbb95a1b3dfa
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2022-07-31 13:37:52 +0100

    Added type-declarations

--- frontend/src/redux/slices/productSlice.ts
+++ frontend/src/redux/slices/productSlice.ts
@@ -15,57 +15,54 @@ const initialState: ProductState = {
     error: null
 };
 
-export const productSlice = createSlice({
-    name: "product",
-    initialState,
-    reducers: {
-        listProducts: (state, action: PayloadAction<ProductItem[]>) => {
-            state.products = action.payload;
-        },
-        listProductDetails: (state, action: PayloadAction<ProductItem>) => {
-            state.product = action.payload;
-        }
-    },
-    extraReducers: (builder) => {
-        builder.addCase(listProductRequest.pending, (state, action) => {
-            state.loading = true;
-        });
-        builder.addCase(listProductRequest.fulfilled, (state, action) => {
-            state.loading = false;
-        });
-        builder.addCase(listProductRequest.rejected, (state, action) => {
-            state.loading = false;
-            if (action.error.message) {
-                state.error = action.error.message;
-            }
-        });
-        builder.addCase(listProductDetailsRequest.pending, (state, action) => {
-            state.loading = true;
-        });
-        builder.addCase(listProductDetailsRequest.fulfilled, (state, action) => {
-            state.loading = false;
-        });
-        builder.addCase(listProductDetailsRequest.rejected, (state, action) => {
-            state.loading = false;
-            if (action.error.message) {
-                state.error = action.error.message;
-            }
-        });
+export const listProducts = createAsyncThunk<ProductItem[], void>(
+    "product/listProducts",
+    async () => {
+        const { data } = await axios.get(`/api/products`);
+        return data as ProductItem[];
     }
-});
+);
 
-export const { listProducts, listProductDetails } = productSlice.actions;
-export default productSlice.reducer;
-
-export const listProductRequest = createAsyncThunk("product/listProducts", async () => {
-    const { data } = await axios.get(`/api/products`);
-    return data;
-});
-
-export const listProductDetailsRequest = createAsyncThunk(
+export const listProductDetails = createAsyncThunk<ProductItem, string>(
     "product/listProductDetails",
     async (id) => {
         const { data } = await axios.get(`/api/products/${id}`);
-        return data;
+        return data as ProductItem;
     }
 );
+
+export const productSlice = createSlice({
+    name: "product",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(listProducts.pending, (state) => {
+            state.loading = true;
+        });
+        builder.addCase(listProducts.fulfilled, (state, action) => {
+            state.loading = false;
+            state.products = action.payload;
+        });
+        builder.addCase(listProducts.rejected, (state, action) => {
+            state.loading = false;
+            if (action.error.message) {
+                state.error = action.error.message;
+            }
+        });
+        builder.addCase(listProductDetails.pending, (state) => {
+            state.loading = true;
+        });
+        builder.addCase(listProductDetails.fulfilled, (state, action) => {
+            state.loading = false;
+            state.product = action.payload;
+        });
+        builder.addCase(listProductDetails.rejected, (state, action) => {
+            state.loading = false;
+            if (action.error.message) {
+                state.error = action.error.message;
+            }
+        });
+    }
+});
+
+export default productSlice.reducer;
