3ba61e4a7526757586ba55f2ac76710bd9c680a6
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2022-07-31 13:15:01 +0100

    Renamed features -> slices

--- frontend/src/redux/slices/authSlice.ts
+++ frontend/src/redux/slices/authSlice.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 
 interface AuthState {
     user: UserAuthData | null;
-    loading: boolean;
-    error: string | null;
+    loading?: boolean;
+    error?: string | null;
 }
 
 const initialState: AuthState = {
