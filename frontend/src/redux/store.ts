d7cd0ee0b6e2ed9c0341eb649a17c2cb43abb021
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2022-07-31 11:03:24 +0100

    Load initial state from localStorage

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,10 +1,39 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import userReducer from "./features/userSlice";
 import cartReducer from "./features/cartSlice";
+import orderReducer from "./features/orderSlice";
+import productReducer from "./features/productSlice";
+
+// Check local storage for cartItems
+// If cartItems are present, set the initial state to the values saved in local storage
+const cartItems: string | null = localStorage.getItem("cartItems");
+const cartItemsFromLocalStorage = cartItems != null ? JSON.parse(cartItems) : [];
+
+// Check local storage for userInfo
+// If userInfo is set, set the initial state to the values saved in local
+const userInfo: string | null = localStorage.getItem("userInfo");
+const userInfoFromLocalStorage = userInfo != null ? JSON.parse(userInfo) : null;
+
+// Check local storage for shippingAddress
+// If shippingAddress is set, set the initial state to the values saved in local storage
+const shippingAddress = localStorage.getItem("shippingAddress");
+const shippingAddressFromLocalStorage = shippingAddress ? JSON.parse(shippingAddress) : {};
 
 export const store = configureStore({
     reducer: {
-        cart: cartReducer
+        cart: cartReducer,
+        order: orderReducer,
+        product: productReducer
+    },
+    preloadedState: {
+        cart: {
+            cartItems: cartItemsFromLocalStorage,
+            shippingAddress: shippingAddressFromLocalStorage,
+            paymentMethod: ""
+        }
+        // userLogin: {
+        //     userInfo: userInfoFromLocalStorage
+        // }
     }
 });
 
