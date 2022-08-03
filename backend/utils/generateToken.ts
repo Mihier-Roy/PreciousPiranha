7544b8febc8852ab1c5b3613b215557dce98be12
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2022-08-03 12:56:04 +0100

    Defined payload type

--- backend/utils/generateToken.ts
+++ backend/utils/generateToken.ts
@@ -3,5 +3,5 @@ import crypto from "crypto";
 
 const key = process.env.JWT_SECRET || crypto.randomBytes(20).toString("hex");
 export const generateToken = (id: string) => {
-    return jwt.sign({ id: id }, key, { algorithm: "HS512" });
+    return jwt.sign({ id: id } as TokenInterface, key, { algorithm: "HS512" });
 };
