067d76cd62225dc9a6d6f450b646803c047b7562
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2022-08-03 13:40:20 +0100

    Migrated to typescript

--- backend/controllers/productController.ts
+++ backend/controllers/productController.ts
@@ -1,5 +1,5 @@
 import asyncHandler from "express-async-handler";
-import Product from "../models/productModel.js";
+import Product from "../models/productModel";
 
 // Description 	: Returns all products
 // Route 		: GET /api/products
