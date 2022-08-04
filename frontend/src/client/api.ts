448b301e0e8afd841b9b2b174940b20c3d191579
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2022-08-04 11:07:58 +0100

    Re-factored to create a single axios instance which can be re-used

--- frontend/src/client/api.ts
+++ frontend/src/client/api.ts
@@ -1,23 +1,28 @@
 import axios from "axios";
 
-axios.defaults.headers.common["Content-Type"] = `application/json`;
+// Set config defaults when creating the instance
+const instance = axios.create({
+    headers: {
+        "Content-Type": `application/json`
+    }
+});
 
 export const setToken = (token: string) => {
-    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 };
 
 export const getRequest = async (url: string) => {
-    return await axios.get(url);
+    return await instance.get(url);
 };
 
 export const postRequest = async (url: string, data: object) => {
-    return await axios.post(url, data);
+    return await instance.post(url, data);
 };
 
 export const putRequest = async (url: string, data: object) => {
-    return await axios.put(url, data);
+    return await instance.put(url, data);
 };
 
 export const deleteRequest = async (url: string) => {
-    return await axios.delete(url);
+    return await instance.delete(url);
 };
